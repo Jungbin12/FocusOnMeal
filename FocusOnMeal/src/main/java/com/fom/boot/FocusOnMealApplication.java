a81003755e777f86e92b5d533a33b6dcabf61d0d
ho-woo <dofvm1004@gmail.com>
2025-11-03 12:50:24 +0900

    [INIT] FocusOnMeal 기본 파일 구조 생성

---
+++ FocusOnMeal/src/main/java/com/fom/boot/FocusOnMealApplication.java
@@ -0,0 +1,13 @@
+package com.fom.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusOnMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusOnMealApplication.class, args);
+	}
+
+}
