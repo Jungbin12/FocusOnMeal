4e12a982bc734ce8f45c28420f1cae69a160d91d
zhzhzlajava <zhzhzlajava@gmail.com>
2025-11-13 16:35:03 +0900

    [MODIFY] Mypage 내 Dashboard 파일 수정
    - 왼쪽 사이드바 및 css 추가

--- FocusOnMeal/src/main/front-end/src/pages/mypage/Dashboard.jsx
+++ FocusOnMeal/src/main/front-end/src/pages/mypage/Dashboard.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import Header from "../../components/common/Header";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import styles from "./Dashboard.module.css";
+import Sidebar from "../../components/mypage/Sidebar";
 
 const Dashboard = () => {
-    return(
+    return (
         <>
+            <div className={styles.container}>
+            <Sidebar />
+                {/* 메인 영역 */}
+                <main className={styles.main}>
+                    <h2>닉네임님 환영합니다!</h2>
+                </main>
+
+            </div>
         </>
-    )
-}
+    );
+};
 
 export default Dashboard;
