ade3dd5f99e3d44f3856071cca931355808d9150
zhzhzlajava <zhzhzlajava@gmail.com>
2025-11-14 14:13:09 +0900

    [MODIFY] 사이드바 css 수정

--- FocusOnMeal/src/main/front-end/src/components/admin/Sidebar.jsx
+++ FocusOnMeal/src/main/front-end/src/components/admin/Sidebar.jsx
@@ -7,27 +7,43 @@ const Sidebar = () => {
         <aside className={styles.sidebar}>
             <ul className={styles.menuList}>
 
+                {/* 관리자 홈 */}
                 <li>
                     <NavLink
                         to="/admin"
+                        end
                         className={({ isActive }) =>
-                            isActive ? styles.active : styles.menuTitle
-                        }>
-                        관리자 홈
+                            isActive ? styles.activeTitle : styles.menuTitle
+                        }
+                    >
+                        홈
                     </NavLink>
                 </li>
 
-                <li className={styles.menuTitle}>
+                {/* 회원 관리 */}
+                <li>
                     <NavLink
                         to="/admin/memberInfo"
                         className={({ isActive }) =>
-                            isActive ? styles.active : styles.menuTitle
-                        }>
+                            isActive ? styles.activeTitle : styles.menuTitle
+                        }
+                    >
                         회원 관리
                     </NavLink>
                 </li>
 
-                <li className={styles.menuTitle}>데이터 관리</li>
+                {/* 데이터 관리 */}
+                <li>
+                    <NavLink
+                        to="/admin/safetyInfo"
+                        className={({ isActive }) =>
+                            isActive ? styles.activeTitle : styles.menuTitle
+                        }
+                    >
+                        데이터 관리
+                    </NavLink>
+                </li>
+
                 <li>
                     <NavLink
                         to="/admin/safetyInfo"
@@ -38,6 +54,7 @@ const Sidebar = () => {
                         안전 정보 관리
                     </NavLink>
                 </li>
+
                 <li>
                     <NavLink
                         to="/admin/ingredientInfo"
@@ -49,16 +66,29 @@ const Sidebar = () => {
                     </NavLink>
                 </li>
 
-                <li className={styles.menuTitle}>
+                {/* 공지사항 관리 */}
+                <li>
                     <NavLink
                         to="/admin/noticeInfo"
                         className={({ isActive }) =>
-                            isActive ? styles.active : styles.menuTitle
-                        }>
+                            isActive ? styles.activeTitle : styles.menuTitle
+                        }
+                    >
                         공지사항 관리
                     </NavLink>
                 </li>
-                <li className={styles.menuTitle}>배치 / 로그</li>
+
+                {/* 배치 */}
+                <li>
+                    <NavLink
+                        to="/admin/batch"
+                        className={({ isActive }) =>
+                            isActive ? styles.activeTitle : styles.menuTitle
+                        }
+                    >
+                        배치 / 로그
+                    </NavLink>
+                </li>
             </ul>
         </aside>
     );
